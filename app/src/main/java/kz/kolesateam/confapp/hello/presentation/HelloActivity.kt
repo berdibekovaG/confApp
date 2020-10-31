01760ed4a03c9003a93bb772a34202043f69731f
gaukharB <berdibekova.gaukhar@gmail.com>
2020-11-01 03:49:41 +0600

    прописана логика приложения, доработаю

--- app/src/main/java/kz/kolesateam/confapp/hello/presentation/HelloActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/hello/presentation/HelloActivity.kt
@@ -1,26 +1,36 @@
 package kz.kolesateam.confapp.hello.presentation
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import kz.kolesateam.confapp.APPLICATION_SHARED_PREFERENCES
 import kz.kolesateam.confapp.R
+import kz.kolesateam.confapp.USER_NAME_KEY
 
 private const val TAG = "HelloActivity"
 
-class HelloActivity : AppCompatActivity() {
-
-    private val closeHelloButton: Button by lazy {
-        findViewById(R.id.activity_hello_close_hello_button)
-    }
+class HelloActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello)
 
-        closeHelloButton.setOnClickListener {
-            finish()
-        }
+        val nameText: TextView = findViewById(R.id.activity_hello_name_text)
+        val userName: String = getSavedUserName()
+        nameText.text = resources.getString(R.string.hello_user_fmt, userName)
+    }
 
+    private fun getSavedUserName(): String{
+
+        val sharedPreferences: SharedPreferences = getSharedPreferences(
+                APPLICATION_SHARED_PREFERENCES,
+                Context.MODE_PRIVATE
+        )
+
+        return sharedPreferences.getString(USER_NAME_KEY, "Мир") ?: "Мир"
     }
 }
