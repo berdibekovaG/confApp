73b6d1e149f4a0be9af441cc37b730db840d08af
Harosich1 <godvla91@gmail.com >
2020-11-15 23:01:59 +0600

    добавлена кнопка которая ведет на upcomingEventsActivity, а так же для неё сверстан экран

--- app/src/main/java/kz/kolesateam/confapp/HelloActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/HelloActivity.kt
@@ -1,21 +1,11 @@
 package kz.kolesateam.confapp
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import kz.kolesateam.confapp.hello.presentation.TestHelloActivity
-import kz.kolesateam.confapp.presentation.common.AbstractTextWatcher
+import kz.kolesateam.confapp.events.presentation.UpcomingEventsActivity
 
-const val USER_NAME_KEY = "user_name"
-const val APPLICATION_SHARED_PREFERENCES = "application"
-private const val TAG = "TestHelloActivity"
 
 class HelloActivity : AppCompatActivity() {
 
@@ -24,32 +14,14 @@ class HelloActivity : AppCompatActivity() {
         setContentView(R.layout.activity_hello)
 
         val mainContinueButton: Button = findViewById(R.id.continue_button)
-        val mainYourNameTextView: EditText = findViewById(R.id.enter_your_name_textView)
-
-        mainYourNameTextView.addTextChangedListener(
-
-                AbstractTextWatcher { text ->
-                    mainContinueButton.isEnabled = text.isNotBlank()
-                }
-
-        )
 
         mainContinueButton.setOnClickListener {
-            saveUser(mainYourNameTextView.text.toString())
-            navigateToHelloScreen()
+            navigateToUpcomingEventsActivity()
         }
     }
 
-    private fun saveUser(userName: String) {
-        val sharedPreferences: SharedPreferences = getSharedPreferences(APPLICATION_SHARED_PREFERENCES, Context.MODE_PRIVATE)
-        val editor: SharedPreferences.Editor = sharedPreferences.edit()
-
-        editor.putString(USER_NAME_KEY, userName)
-        editor.apply()
-    }
-
-    private fun navigateToHelloScreen() {
-        val helloScreenIntent = Intent(this, TestHelloActivity::class.java)
+    private fun navigateToUpcomingEventsActivity() {
+        val helloScreenIntent = Intent(this, UpcomingEventsActivity::class.java)
         startActivity(helloScreenIntent)
     }
 
