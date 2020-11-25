babfc2b62bd4aa5375ecc4f7b352843e0b6bd1ca
Nellya <kebirova@kolesa.kz>
2020-11-25 16:59:07 +0600

    Выпилила ненужные файлы

--- app/src/main/java/kz/kolesateam/confapp/HelloActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/HelloActivity.kt
@@ -1,10 +1,7 @@
 package kz.kolesateam.confapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import kz.kolesateam.confapp.events.presentation.UpcomingEventsActivity
 
 
 class HelloActivity : AppCompatActivity() {
@@ -12,17 +9,5 @@ class HelloActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello)
-
-        val mainContinueButton: Button = findViewById(R.id.continue_button)
-
-        mainContinueButton.setOnClickListener {
-            navigateToUpcomingEventsActivity()
-        }
     }
-
-    private fun navigateToUpcomingEventsActivity() {
-        val helloScreenIntent = Intent(this, UpcomingEventsActivity::class.java)
-        startActivity(helloScreenIntent)
-    }
-
 }
